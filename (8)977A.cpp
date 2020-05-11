e1185091ad2c2617182a3ac9c6ec4bcb8958ef88
Nurul Ahad Farhan <61065246+farhanahad@users.noreply.github.com>
2020-05-12 00:28:04 +0600

    Add files via upload

---
+++ (8)977A.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,n;
+    cin>>a>>n;
+    while(n--)
+    {
+        if(a%10==0)
+            a=a/10;
+        else
+            a--;
+    }
+    cout<<a;
+}
