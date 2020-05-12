19ccd2ac7cc45deb12ada09dfcd17541439a2340
Nurul Ahad Farhan <61065246+farhanahad@users.noreply.github.com>
2020-05-13 02:19:56 +0600

    Add files via upload

---
+++ (14)282A(A. Bit++).cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i,x=0;
+    string s;
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+        cin>>s;
+          if(s[1]=='+')
+            x++;
+        else
+            x--;
+
+    }
+    cout<<x;
+    }
+
