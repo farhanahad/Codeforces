77ee3d861e79c5fa1932a078a85e9eaaa7780ee2
Nurul Ahad Farhan <61065246+farhanahad@users.noreply.github.com>
2020-05-19 11:17:34 +0600

    Add files via upload

---
+++ (27)133A(A. HQ9+).cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string s;
+    int i;
+    cin>>s;
+    for(i=0;i<s.size();i++)
+    {
+        if(s[i]=='H' || s[i]=='Q' || s[i]=='9')
+            {
+                cout<<"YES";
+                return 0;
+            }
+    }
+    cout<<"NO";
+}
